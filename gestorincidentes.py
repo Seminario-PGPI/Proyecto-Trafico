dd122381b784375c657dedfb9c8c67d954242de8
mellado <mellado@pablomellado.es>
2019-10-30 23:20:36 +0100

    Adición de primera clase GestorIncidentes #3

--- gestorincidentes.py
+++ gestorincidentes.py
@@ -1,16 +1,44 @@
 from incidente import Incidente
+"""
+Las dos esquinas del recuadro que incluye Granada son:
+Latitud: 37.234947 | Longitud: -3.731565
+Latitud: 37.113558 | Longitud: -3.455327
+"""
+MAX_LATITUD = 37.234947
+MIN_LATITUD = 37.113558
+MAX_LONGITUD = -3.455327
+MIN_LONGITUD = -3.731565
 
 # Clase entidad
 class GestorIncidentes(object):
     
     incidentes = []
-    def addIncidente(incidente):
+    def addIncidente(self, incidente: Incidente):
+        if not self.checkLongitudLatitud(incidente.latitud, incidente.longitud):
+            raise LocationOutOfGranada
         self.incidentes.append(incidente)
     
-    def getIncidente(latitud, longitud):
+    def getIncidente(self, latitud: float, longitud: float) -> list:
+        if not self.checkLongitudLatitud(latitud, longitud):
+            raise LocationOutOfGranada
         result = []
         for incidente in incidentes:
-            if incidente["Latitud"] == latitud and incidente["Longitud"] == longitud:
+            if incidente.latitud == latitud and incidente.longitud == longitud:
                 result.append(incidente)
         return result
+    
+    def checkLongitudLatitud(self, latitud: float, longitud: float) -> bool:
         
+        if latitud > MAX_LATITUD:
+            return False
+        if latitud < MIN_LATITUD:
+            return False
+        if longitud > MAX_LONGITUD:
+            return False
+        if longitud < MIN_LONGITUD:
+            return False
+        return True
+
+
+class LocationOutOfGranada(Exception):
+    pass
