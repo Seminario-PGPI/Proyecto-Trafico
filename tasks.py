93c782050f21ea936de4b32a9568bb863925b424
Oscar Rubio Garcia <takitow33@hotmail.com>
2019-11-05 14:25:09 +0100

    Actualizacion del fichero tasks.py, con definicion de Task

--- tasks.py
+++ tasks.py
@@ -1,4 +1,13 @@
 from invoke import run
-cmd = "pip install -r requirements.txt"
-result = run(cmd, hide=True, warn=True)
-print(result.ok)
+from invoke import task
+
+@task
+def build(clean=False):
+    cmd = "pip install -r requirements.txt"
+    result = run(cmd, hide=True, warn=True)
+    print(result.ok)
+
+@task
+def testAll(ctx):
+    ctx.run("python3 -m unittest discover -s ./tests -p 'test*.py'")
+
